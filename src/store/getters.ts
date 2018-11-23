46c504246f481196b00b24e4f65cae951d5f7b02
connor <c.stroomberg@gmail.com>
2018-11-23 16:25:58 +0100

    link selectedOptions to enabled state of dataItem

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -7,6 +7,10 @@ const lookup = <T extends Identifiable>(ids: string[], options: T[]): T[] =>
     ids.map(id => (options || []).find(option => option.id === id))
     .filter((x => x !== undefined) as TermGuard<T>)
 
+const intersects = (a: string[], b: string[]): boolean => {
+  return a.filter(value => -1 !== b.indexOf(value)).length > 0
+}
+
 export default {
   topicTree: (state: ApplicationState): VueTopic[] => {
     const isRootTopic = (topic: Topic): boolean => !topic.parentTopicId || topic.parentTopicId === topic.id
@@ -31,7 +35,10 @@ export default {
       sexGroups: lookup(item.sexGroups, state.categoricalFacets.sexGroup.options),
       topic: lookup([item.topic], state.topics)[0],
       selected: state.selectedDataItems.includes(item.id),
-      enabled: true
+      enabled: intersects(state.selectedOptions.subCohorts, item.subCohorts) ||
+          intersects(state.selectedOptions.ageGroup, item.ageGroups) ||
+          intersects(state.selectedOptions.sexGroup, item.sexGroups) ||
+          intersects(state.selectedOptions.collectionPoint, item.collectionPoints)
     })),
   selectedAgeGroups: (state: ApplicationState): string[] => state.selectedOptions.ageGroup,
   selectedSexGroups: (state: ApplicationState): string[] => state.selectedOptions.sexGroup,
